d1c78ae912e99aa7904b5ea4f4623e133c757395
Alex de Mulder <alexdemulder@gmail.com>
2018-03-05 11:53:37 +0100

    added simple post printing

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.post('/', (req, res) => {
+  console.log("req.body",req.body);
+  res.send('Done');
+})
+
+app.listen(4000, () => console.log('Example app listening on port 4000!'))
